85e1f8c777de60001bd6ba03ea118770b3fce7e2
crecket <crecketgaming@gmail.com>
2016-06-16 22:44:12 +0200

    config changes

--- src/server/configs/config-template.js
+++ src/server/configs/config-template.js
@@ -12,54 +12,22 @@ config.sslOptions = {};
 
 // check if online host name is found
 if (os.hostname().trim() === config.onlineHostName) {
+    // online settings, we also need to supply the ca certificate chain
     config.sslOptions = {
         key: fs.readFileSync('/etc/letsencrypt/live/example.com/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/example.com/cert.pem'),
         // ca is required in most cases or some devices will decline the ssl certificate
-        ca: [fs.readFileSync('/etc/letsencrypt/live/example.com/chain.pem')],
-        ciphers: [
-            "ECDHE-RSA-AES256-SHA384",
-            "DHE-RSA-AES256-SHA384",
-            "ECDHE-RSA-AES256-SHA256",
-            "DHE-RSA-AES256-SHA256",
-            "ECDHE-RSA-AES128-SHA256",
-            "DHE-RSA-AES128-SHA256",
-            "HIGH",
-            "!aNULL",
-            "!eNULL",
-            "!EXPORT",
-            "!DES",
-            "!RC4",
-            "!MD5",
-            "!PSK",
-            "!SRP",
-            "!CAMELLIA"
-        ].join(':'),
+        ca: [fs.readFileSync('/etc/letsencrypt/live/example.com/cachain.pem')],
+        ciphers: ["ECDHE-RSA-AES256-SHA384", "DHE-RSA-AES256-SHA384", "ECDHE-RSA-AES256-SHA256", "DHE-RSA-AES256-SHA256", "ECDHE-RSA-AES128-SHA256", "DHE-RSA-AES128-SHA256", "HIGH", "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!SRP", "!CAMELLIA"].join(':'),
         honorCipherOrder: true,
         requestCert: false
     };
 } else {
+    // local settings, no CA is required since we use self signed certificates
     config.sslOptions = {
         key: fs.readFileSync('src/server/certs/example.com.key'),
         cert: fs.readFileSync('src/server/certs/domain.crt'),
-        ciphers: [
-            "ECDHE-RSA-AES256-SHA384",
-            "DHE-RSA-AES256-SHA384",
-            "ECDHE-RSA-AES256-SHA256",
-            "DHE-RSA-AES256-SHA256",
-            "ECDHE-RSA-AES128-SHA256",
-            "DHE-RSA-AES128-SHA256",
-            "HIGH",
-            "!aNULL",
-            "!eNULL",
-            "!EXPORT",
-            "!DES",
-            "!RC4",
-            "!MD5",
-            "!PSK",
-            "!SRP",
-            "!CAMELLIA"
-        ].join(':'),
+        ciphers: ["ECDHE-RSA-AES256-SHA384", "DHE-RSA-AES256-SHA384", "ECDHE-RSA-AES256-SHA256", "DHE-RSA-AES256-SHA256", "ECDHE-RSA-AES128-SHA256", "DHE-RSA-AES128-SHA256", "HIGH", "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!SRP", "!CAMELLIA"].join(':'),
         honorCipherOrder: true,
         requestCert: false
     };
