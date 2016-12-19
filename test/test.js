a4126a2e8ec38a4b638c8e6a767dceaae1024694
Gregory Goijaerts <crecketgaming@gmail.com>
2016-12-19 12:40:17 +0100

    attempt dirname fix

--- test/test.js
+++ test/test.js
@@ -1,101 +1,93 @@
 var assert = require('assert');
 
-var Db;
-var config;
-var userManagement;
+describe('Application', function () {
+    var Db;
+    var config;
+    var userManagement;
 
-// userManagement.users.newUser('crecket', '1234', (success) => {
-//     console.log(success);
-//     if(success){
-//         userManagement.users.removeUser('crecket', (success) => {
-//             console.log(success);
-//         })
-//     }
-// })
-
-
-describe('Config', function () {
-    it('Load config file', function () {
-        // Load the config file
-        config = require('../src/server/configs/config');
-    });
-});
-
-describe('Db', function () {
-    it('Create database handler', function () {
-        // Attempt to create database object
-        Db = require('../src/server/Db')(config);
+    describe('Config', function () {
+        it('Load config file', function () {
+            // Load the config file
+            config = require(__dirname + '/../src/server/configs/config.js');
+        });
     });
 
-    describe('Run database queries', function () {
-
-        it('Select users from db', function () {
-            return new Promise(function (resolve) {
-                // run the query
-                Db.run('SELECT 12* FROM users',
-                    function (result, err) {
-                        // assert we got no error
-                        assert.ok(!err);
-
-                        // resolve the promise
-                        resolve();
-                    }
-                );
-            });
+    describe('Db', function () {
+        it('Create database handler', function () {
+            // Attempt to create database object
+            Db = require(__dirname + '/../src/server/Db.js')(config);
         });
 
-        it('Insert a new user', function () {
-            return new Promise(function (resolve) {
-                // run the query
-                Db.run('INSERT INTO users (username, password, salt) VALUES (?, ?, ?)',
-                    ['crecket', '1234', 'salt'],
-                    function (result, err) {
-                        // assert we got no error
-                        assert.ok(!err);
+        describe('Run database queries', function () {
 
-                        // resolve the promise
-                        resolve();
-                    }
-                );
+            it('Select users from db', function () {
+                return new Promise(function (resolve) { 
+                    // run the query
+                    Db.run('SELECT * FROM users',
+                        function (result, err) {
+                            // assert we got no error
+                            assert.ok(!err);
+
+                            // resolve the promise
+                            resolve();
+                        }
+                    );
+                });
+            });
+
+            it('Insert a new user', function () {
+                return new Promise(function (resolve) {
+                    // run the query
+                    Db.run('INSERT INTO users (username, password, salt) VALUES (?, ?, ?)',
+                        ['crecket', '1234', 'salt'],
+                        function (result, err) {
+                            // assert we got no error
+                            assert.ok(!err);
+
+                            // resolve the promise
+                            resolve();
+                        }
+                    );
+                });
             });
         });
     });
-});
 
-describe('Usermanager', function () {
-    it('Create the usermanagement object', function () {
-        userManagement = require('../src/server/user_management')(Db);
-    })
-
-    describe('loadUsers', function () {
-        it('Load the users from the database', function () {
-            userManagement.users.loadUsers();
+    describe('Usermanager', function () {
+        it('Create the usermanagement object', function () {
+            userManagement = require(__dirname + '/../src/server/user_management.js')(Db);
         })
-    })
 
-    describe('newUser', function () {
-        it('Insert a new user with the hashed password', function () {
-            return new Promise(function (resolve) {
-                userManagement.users.newUser('testusername123', '1234', function (success) {
-                    // assert we got no error
-                    assert.ok(success);
+        describe('loadUsers', function () {
+            it('Load the users from the database', function () {
+                userManagement.users.loadUsers();
+            })
+        })
 
-                    // resolve the promise
-                    resolve();
+        describe('newUser', function () {
+            it('Insert a new user with the hashed password', function () {
+                return new Promise(function (resolve) {
+                    userManagement.users.newUser('testusername123', '1234', function (success) {
+                        // assert we got no error
+                        assert.ok(success);
+
+                        // resolve the promise
+                        resolve();
+                    })
                 })
             })
         })
-    })
 
-    describe('removeUser', function () {
-        it('Remove user', function () {
-            return new Promise(function (resolve) {
-                userManagement.users.removeUser('testusername123', function (success) {
-                    // assert we got no error
-                    assert.ok(success);
+        describe('removeUser', function () {
+            it('Remove user', function () {
+                return new Promise(function (resolve) {
+                    userManagement.users.removeUser('testusername123', function (success) {
+                        // assert we got no error
+                        assert.ok(success);
 
-                    // resolve the promise
-                    resolve();
+                        // resolve the promise
+                        resolve();
+                    })
                 })
             })
         })
